a49dd6a1d3673d2a6b43892f4cbaa26d7ea114b5
Your Name <759859479@qq.com>
2018-06-29 17:08:19 +0800

    6-29B组

--- demo/2018-6-29-B组-js7/js5/app.js
+++ demo/2018-6-29-B组-js7/js5/app.js
@@ -1,10 +1,8 @@
 var myApp = angular.module('myApp', ["ui.router"]);
 
-myApp.constant('A', '新垣结衣');    //方法定义全局变量  ，可以是字符串
-myApp.constant('A', {"a":"新垣结衣","b":"新垣结衣"});    //方法定义全局变量  ，可以是对象
-
+// myApp.constant('A', '新垣结衣');    //方法定义全局变量  ，可以是字符串
+myApp.constant('A', {'a':"新垣结衣",'b':"石原里美"});    //方法定义全局变量  ，可以是对象
 myApp.config(['$stateProvider', '$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
-    
     // $locationProvider.hashPrefix('');
     $urlRouterProvider.otherwise("/login");
     $stateProvider
@@ -22,15 +20,5 @@ myApp.config(['$stateProvider', '$urlRouterProvider',function ($stateProvider, $
             url: "/welcome",
             templateUrl: "tpls/welcome.html"
         })
-        .state("list.option1",{
-            url:"/option1?title&type&img&content&url&id",
-            templateUrl:"tpls/option1.html",
-            controller:"option1Controller"
-        })
-        .state("list.option2",{
-            url:"/option2?page&id",
-            templateUrl:"tpls/option2.html",
-            controller:"option2Controller"
-        })
-        
+     
 }]);
